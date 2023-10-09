a5a538ed32dfb437f6072e0c8c43e6efbd282881
Jason Mendoza <mernstackdev729@gmail.com>
2023-10-08 23:01:32 -0400

    chore: added middleware to check auth, if not redirect to northtensor

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<any>({ req, res })
+  const {
+    data: { session }
+  } = await supabase.auth.getSession()
+
+  if (session) {
+    return res
+  }
+
+  return NextResponse.redirect(
+    `${process.env.NEXT_PUBLIC_NORTHTENSOR_URL}/profile?redirectUrl=${process.env.NEXT_PUBLIC_WIKITENSOR_URL}`
+  )
+}
